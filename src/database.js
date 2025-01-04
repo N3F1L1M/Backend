98596f064a850ed2ad553e9b12665fdb20337ad3
N3F1L1M <cordovaluis0311@gmail.com>
2025-01-04 17:57:03 -0500

    Conexion de la base de datos MongoDb

---
+++ src/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+
+console.log(process.env.MONGODB_URU);
+const URI = process.env.MONGODB_URU;
+
+
+
+mongoose.connect(URI, {
+  //useNewUrlParser: true,
+  //useCreateIndex: true 
+});
+
+ const connection = mongoose.connection;
+ connection.once('open', () => {
+   console.log("DB is connected");
+ });
