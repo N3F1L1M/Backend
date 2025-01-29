47f696c1d42b021baecd9292f0148641c9f09a45
N3F1L1M <cordovaluis0311@gmail.com>
2025-01-29 13:06:00 -0500

    Plantillas de routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 const express = require("express");
-// import express from 'express'
 const cors = require("cors");
 const app = express();
+
+
 
 //setting
 app.set("port", process.env.PORT || 4000);
 
+
+
 //middlewares
 app.use(cors());
 app.use(express.json());
+
+
+
+
 //routes
+app.get('/api/users', (req, res) => res.send('Users Routes'));
+app.get('/api/Notes', (req, res) => res.send('Notes Routes'));
 
 module.exports = app;
