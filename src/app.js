2609bb9b030051dbc95524af02fbfd43c5dad4c8
N3F1L1M <cordovaluis0311@gmail.com>
2025-01-29 12:54:43 -0500

    configuracion del midelwares

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 // import express from 'express'
-
+const cors = require("cors");
 const app = express();
 
 //setting
-app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.PORT || 4000);
 
 //middlewares
-
+app.use(cors());
+app.use(express.json());
 //routes
 
 module.exports = app;
