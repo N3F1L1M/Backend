3204554cc14613cb51fde5bbab128c1e0c66c251
N3F1L1M <cordovaluis0311@gmail.com>
2025-02-13 19:37:51 -0500

    hola

--- src/database.js
+++ src/database.js
@@ -5,7 +5,7 @@ console.log(process.env.MONGODB_URU);
 const URI = process.env.MONGODB_URU || 1;
 
 
-
+/* 
 mongoose.connect(URI, {
   //useNewUrlParser: true,
   //useCreateIndex: true 
@@ -14,4 +14,4 @@ mongoose.connect(URI, {
  const connection = mongoose.connection;
  connection.once('open', () => {
    console.log("DB is connected");
- });
+ }); */
